0f55191b25bfaa51c276d0d45c143233dbfaa5b6
Jakub Bouček <pan@jakubboucek.cz>
2018-06-21 17:58:12 +0200

    Publish repo to GitHub (yep, clean code :)

--- src/Code.js
+++ src/Code.js
@@ -1,3 +1,6 @@
+var title = 'DEPO2015';
+var subtitle = 'Využívání Coworkingu';
+
 var COL_FROM = 1;
 var COL_TO = 2;
 var COL_FROM_REC = 4;
@@ -6,39 +9,45 @@ var COL_SUM = 3;
 
 function doGet() {
   var html = HtmlService.createHtmlOutputFromFile('Index');
-  html.setTitle('Využívání Coworkingu DEPO2015');
+  html.setTitle(title + " - " + subtitle);
   html.setFaviconUrl('https://cdn.jakub-boucek.cz/static/projects/depo2015-coworking/favicon.ico');
   html.addMetaTag('viewport', 'width=device-width, initial-scale=1');
   return html;
 }
 
+function getStatus() {
+  return {
+    isBorrowed: isBorrowed(),
+    title: title,
+    subtitle: subtitle,
+  }
+}
+
 function isBorrowed() {
   var row = getLastRow();
-  
   return row.getCell(1, COL_TO).isBlank();
 }
 
 function addNewDate(isoDate, direct) {
   var date = new Date(isoDate);
   var borrowed = isBorrowed();
-  
-  if((direct == 'return') != borrowed) {
+
+  if ((direct == 'return') != borrowed) {
     throw new Error("Stav neodpovídá požadavku");
   }
-  
-  if(borrowed) {
+
+  if (borrowed) {
     setReturnDate(date);
-  }
-  else {
+  } else {
     setBorrowDate(date);
   }
-  
+
   return !borrowed;
 }
 
 function setReturnDate(date) {
   var row = getLastRow();
-  
+
   row.getCell(1, COL_TO).setValue(date);
   row.getCell(1, COL_TO_REC).setValue(new Date());
 }
@@ -46,13 +55,13 @@ function setReturnDate(date) {
 function setBorrowDate(date) {
   var prev = getLastRow();
   var row = getNewRow();
-  
+
   row.getCell(1, COL_FROM).setValue(date);
   row.getCell(1, COL_FROM_REC).setValue(new Date());
-  
+
   var formula = '=IF(OR(ISBLANK(R[0]C[-2]);ISBLANK(R[0]C[-1]));"";(R[0]C[-1]-R[0]C[-2])*24)';
   row.getCell(1, COL_SUM).setFormulaR1C1(formula);
-  
+
 }
 
 function getNewRow() {
@@ -60,16 +69,26 @@ function getNewRow() {
 }
 
 function getLastRow(next) {
-  var sheet = SpreadsheetApp.getActiveSheet();
+  var sheet = getSheet();
   var lastRow = sheet.getLastRow();
-  
-  if(next) {
+
+  if (next) {
     lastRow++;
   }
 
   var range = sheet.getRange(lastRow, 1, 1, 5);
-  
+
   return range;
 }
 
+function getSheet() {
+  // Pokud je script připojen ke Spreadsheetu, použijte přímé načtení
+  // https://developers.google.com/apps-script/guides/bound
+  var sheet = SpreadsheetApp.getActiveSheet();
 
+  // Pokud máte script napsaný samostatně, využijte tento kód
+  // var spreadsheet = SpreadsheetApp.openByUrl('<-here-put-your-sheet-url>');
+  // var sheet = spreadsheet.getSheets()[0]; // vrátí první sheet zleva
+
+  return sheet;
+}
